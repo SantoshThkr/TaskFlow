961fa0bdc69ab70c6869827383159974e92f7b8d
SantoshThkr <santosh.thakur36911@gmail.com>
2022-01-04 21:08:19 +0530

    feat: implement password hashing

---
+++ backend/app/core/security.py
@@ -0,0 +1,16 @@
+import bcrypt
+
+# bcrypt refuses inputs longer than this, so registration rejects them up front.
+MAX_PASSWORD_BYTES = 72
+
+
+def hash_password(password: str) -> str:
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+
+def verify_password(password: str, password_hash: str) -> bool:
+    try:
+        return bcrypt.checkpw(password.encode(), password_hash.encode())
+    except ValueError:
+        # Malformed stored hash, or a password longer than bcrypt accepts.
+        return False
